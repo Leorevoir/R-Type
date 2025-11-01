7d5e845172366b43dba1b909f4894e64e98e5e66
Mateo Cabrera <mateo.cabrera@epitech.eu>
2025-11-01 19:04:24 +0100

    feat(level2): Implement dynamic asteroid field scenery

--- include/components/map.hpp
+++ include/components/map.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <R-Engine/Maths/Vec.hpp>
+
 struct Background {
         float scroll_speed = 2.0f;
 };
 
 struct ScrollingScenery {
         float scroll_speed = 4.0f;
+};
+
+struct Asteroid {
+        r::Vec3f velocity;
+        r::Vec3f rotation_speed;
 };
